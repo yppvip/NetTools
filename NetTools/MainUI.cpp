b16875f8f25886603c245d8ddb7c0700e70c1292
ypp <1256526069@qq.com>
2023-01-04 20:49:33 +0800

    [feature] 第一次提交

---
+++ NetTools/MainUI.cpp
@@ -0,0 +1,11 @@
+#include "MainUI.h"
+
+MainUI::MainUI(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+MainUI::~MainUI()
+{
+}
+
---
+++ NetTools/MainUI.h
@@ -0,0 +1,14 @@
+#ifndef MAINUI_H
+#define MAINUI_H
+
+#include <QWidget>
+
+class MainUI : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainUI(QWidget *parent = nullptr);
+    ~MainUI();
+};
+#endif // MAINUI_H
